ff2564abe89aaa0848c0b219cc5e104cf2f87c8b
VIXXENT0 <VIXXENT0@gmail.com>
2020-05-01 00:40:45 +0200

    Infinite scroll pagination implemented!
    GridLoader:
    Implemented logic to detect if it is at the bottom
    added several logs to avoid too many re-renders.
    
    useScrollPosition
    Use of this custom hook to detect scroll and manage the event.

--- src/components/body-grid/GridLoader.tsx
+++ src/components/body-grid/GridLoader.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { DocumentNode } from 'apollo-boost';
 import { Thing, Cursor, ThingsCursoredList } from '../../services/apollo/types';
-import { Button, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { QueryResult } from 'react-apollo';
 import GridThingElement from './GridThingElement';
+import useScrollPosition, {PositionChange} from '../util/useScrollPosition';
 
 export interface GridLoaderProps{
     query: DocumentNode,
@@ -14,15 +15,36 @@ export interface thingsQueryData{
     thingsCursoredList: ThingsCursoredList
 }
 
+const isBottom = (positionChange: PositionChange):boolean=>{
+    const currentWindowInnerHeight = window.innerHeight;
+    const distanceScrolledDown = (-positionChange.currentPosition.y);
+    const totalHeight = document.body.scrollHeight;
+    const isBottom = currentWindowInnerHeight + distanceScrolledDown === totalHeight
+
+    return isBottom;
+}
+
 export default function(props:GridLoaderProps){
-    console.log("GRID_LOADER - props      ", props);
+    console.log(`${timeString()} GRID_LOADER - START! - loaded[${props.firstQueryResult.data?.thingsCursoredList.things.length}] - `, props);
     const loadedData = props.firstQueryResult.data;
     const loadedThings:Thing[]|undefined = loadedData?.thingsCursoredList?.things;
-    const [cursor, setCursor] = useState<Cursor>({page:1, per_page:10});
-    const clickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-        nextPage(cursor, setCursor);
-        loadMore(props.firstQueryResult.fetchMore, props.query, cursor);
+    const [cursor, setCursor] = useState<Cursor>({page:1, per_page:50});
+
+    const scrollHandler = (positionChange: PositionChange) => {
+        if(isBottom(positionChange)){
+            console.log(`${timeString()} Bottom reached!`);
+            nextPage(cursor, setCursor);
+            loadMore(props.firstQueryResult.fetchMore, props.query, cursor);
+        }
     }
+
+    useScrollPosition(
+        scrollHandler,
+        [],
+        useRef(document.body),
+        false,
+        200
+    );
 
     let jsxResult;
 
@@ -38,24 +60,23 @@ export default function(props:GridLoaderProps){
         )
     }
     
-    return(
-        <div>
-            {jsxResult}
-            <Button onClick={clickHandler}>(+)</Button>
-        </div>
-    );
+    return jsxResult;
 }
 
 const nextPage = (cursor:Cursor, setCursor: React.Dispatch<React.SetStateAction<Cursor>>)=>{
+    console.log(`${timeString()} GRID_LOADER - NEXT_PAGE - INCREMENT PAGE (${JSON.stringify(cursor)})`);
     cursor.page++;
+    console.log(`${timeString()} GRID_LOADER - NEXT_PAGE - DONE (${JSON.stringify(cursor)})`);
     setCursor(cursor);
 }
 
 const loadMore = (fetchMore:Function, query:DocumentNode, cursor:Cursor) => {
+    console.log(`${timeString()} GRID_LOADER - LOAD_MORE - START! `);
     return fetchMore({
         query,
         variables: { cursor },
         updateQuery: (previousResult:thingsQueryData, { fetchMoreResult }:{fetchMoreResult:thingsQueryData}) => {
+            console.log(`${timeString()} GRID_LOADER - LOAD_MORE - UPDATE_QUERY - START! `);
             const existingResults = previousResult.thingsCursoredList;
             const newResults = fetchMoreResult.thingsCursoredList;
             const result:thingsQueryData = {
@@ -69,4 +90,9 @@ const loadMore = (fetchMore:Function, query:DocumentNode, cursor:Cursor) => {
             return result;
         }
     })
+}
+
+const timeString = ()=>{
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`
 }
---
+++ src/components/util/useScrollPosition.ts
@@ -0,0 +1,66 @@
+import { useRef, MutableRefObject, useLayoutEffect } from 'react';
+
+export interface GetScrollPositionArgument {
+    element?: MutableRefObject<HTMLElement>,
+    useWindow: boolean 
+}
+
+export interface XYPosition{
+    x: number
+    y: number;
+    totalSize:{
+        x:number,
+        y:number
+    }
+}
+
+export interface PositionChange{
+    previousPosition: XYPosition
+    currentPosition: XYPosition
+}
+
+const isBrowser = typeof window !== `undefined`
+
+function getScrollPosition({ element, useWindow }: GetScrollPositionArgument):XYPosition {
+    if (!isBrowser) return { x: 0, y: 0, totalSize:{ x: 0, y: 0 } }
+    const target = element ? element.current : document.body
+    const position = target.getBoundingClientRect()
+
+    return useWindow
+        ? { x: window.scrollX, y: window.scrollY, totalSize:{x:window.innerWidth, y:window.innerHeight} }
+        : { x: position.left, y: position.top, totalSize:{x:target.scrollWidth, y:target.scrollHeight} }
+}
+
+export default function(
+    effect:(positionChange:PositionChange)=>any,
+    deps:any[],
+    element: MutableRefObject<HTMLElement>|undefined,
+    useWindow:boolean,
+    wait:number
+) {
+    const position = useRef(getScrollPosition({ useWindow }))
+    let throttleTimeout:NodeJS.Timeout|null = null
+
+    const callBack = () => {
+        const currentPosition = getScrollPosition({ element, useWindow })
+        effect({ previousPosition: position.current, currentPosition })
+        position.current = currentPosition
+        throttleTimeout = null
+    }
+
+    useLayoutEffect(() => {
+        const handleScroll = () => {
+            if (wait) {
+                if (throttleTimeout === null) {
+                    throttleTimeout = setTimeout(callBack, wait)
+                }
+            } else {
+                callBack()
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, deps)
+}
